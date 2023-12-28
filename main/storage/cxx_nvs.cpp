fddcc129b9fb715567fce6c973451bc46fa83a72
artofcomputing <pedro.mello@artofcomputing.art>
2023-12-28 01:41:04 -0300

    add nvs manager destructor definition and re-design constructor

--- main/storage/cxx_nvs.cpp
+++ main/storage/cxx_nvs.cpp
@@ -19,12 +19,32 @@ NVS::Manager::Manager() {
         spdlog::warn("NVS partition was truncated with code {}, retrying", esp_err_to_name(err));
         // NVS partition was truncated and needs to be erased
         // Retry nvs_flash_init
+        spdlog::debug("Attempting to erase NVS partition");
         if (const int erase{nvs_flash_erase()}; erase != ESP_OK) {
             spdlog::error("Failed to erase NVS partition with code {}, exiting", esp_err_to_name(erase));
         }
+        else {
+            spdlog::debug("NVS partition has been erased");
+            spdlog::debug("Attempting to re-initialize NVS after flashing NVS");
+            err = nvs_flash_init();
+        }
     }
-    if (err = nvs_flash_init(); err != ESP_OK) {
+    if (err != ESP_OK) {
         spdlog::error("Failed to initialize NVS, with code {}, exiting", esp_err_to_name(err));
     }
     spdlog::debug("NVS Initialized");
 }
+
+NVS::Manager::~Manager() {
+    spdlog::debug("Attempting to de-initialize NVS");
+    if (const esp_err_t err{nvs_flash_deinit()}; err != ESP_OK) {
+        spdlog::error("Failed to de-initialize NVS, with code {}, retrying", esp_err_to_name(err));
+        if (const esp_err_t retry_err{nvs_flash_deinit()}; retry_err != ESP_OK) {
+            spdlog::error("Retry NVS initialization failed, with code {}, exiting", esp_err_to_name(retry_err));
+        }
+    }
+    else if (err == ESP_OK) {
+        spdlog::debug("NVS De-initialized");
+    }
+}
+
