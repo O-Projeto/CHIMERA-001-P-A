ea3d2179d332bdc432d69db929dbeb0de277ffd8
artofcomputing <pedro.mello@artofcomputing.art>
2023-12-28 18:32:48 -0300

    Update todo list

--- main/main.cpp
+++ main/main.cpp
@@ -8,7 +8,8 @@
 
 // TODO:
 // - Design Error Handling Subcomponent.
-// - Create Temperature Sensor Monitor, for checking the current operating temperature.
+// - Design Telemetry Subcomponent.
+// - Design Clock Synchronization Subcomponent.
 
 // Main task called by FreeRTOS Scheduler after Application Startup
 // https://docs.espressif.com/projects/esp-idf/en/v5.1.2/esp32/api-guides/startup.html
