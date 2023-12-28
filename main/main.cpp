f72a99a73936e0a8e2d249fffe659f3b2eb4ea89
artofcomputing <pedro.mello@artofcomputing.art>
2023-12-28 01:39:51 -0300

    add nvs note

--- main/main.cpp
+++ main/main.cpp
@@ -13,10 +13,11 @@
 // Main task called by FreeRTOS Scheduler after Application Startup
 // https://docs.espressif.com/projects/esp-idf/en/v5.1.2/esp32/api-guides/startup.html
 extern "C" void app_main(void) {
-
     // Global Logging Level Initialization
     spdlog::set_level(spdlog::level::debug);
 
     // NVS Driver Initialization Block
+    // NOTE: NVS Wrapper Class for the moment DOES NOT apply Error Handling, this will be implemented
+    // after a suitable Error Handling Strategy is defined.
     NVS::Manager();
 }
