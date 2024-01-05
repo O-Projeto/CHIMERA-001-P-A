438b967a1a5927800248e4658c19fc64be168ffd
artofcomputing <pedro.mello@artofcomputing.art>
2024-01-04 21:32:05 -0300

    refactor entrypoint with new console implementation

--- main/main.cpp
+++ main/main.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <cstdio>
 #include <memory>
 
+#include "console/cxx_console.hpp"
 #include "esp_event_cxx.hpp"
 #include "storage/cxx_nvs.hpp"
 #include "exception/cxx_exception.hpp"
@@ -34,9 +35,10 @@ int servo_function(int argc, char** argv) {
 // Main task called by FreeRTOS Scheduler after Application Startup
 // https://docs.espressif.com/projects/esp-idf/en/v5.1.2/esp32/api-guides/startup.html
 extern "C" void app_main(void) {
-    // NVS Driver Initialization Block
+    // CHIMERA subsystem initialization
     try {
-        chimera::nvs();
+        chimera::nvs subsystem_nvs{};
+        chimera::console subsystem_console{};
     }
     // NOTE: Keep this disabled until ReSharper is fixed
     // ReSharper disable once CppDFAUnreachableCode
@@ -45,36 +47,14 @@ extern "C" void app_main(void) {
         abort();
     }
 
-    const std::shared_ptr<esp_console_repl_t *> repl{new esp_console_repl_t *};
-
-    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    repl_config.prompt = "CHIMERA-001-P-A >";
-
-    //Register help command
-    if (const esp_err_t err{esp_console_register_help_command()}; err != ESP_OK) {
-        fprintf(stderr, "Unable to register help command in the console");
-    }
-
     // Register servo command
     constexpr esp_console_cmd_t servo_command{
-        .command = "servo",
-        .help = "Run servo controller",
-        .hint = nullptr,
-        .func = &servo_function,
-        .argtable = nullptr,
+        "servo",
+        "Run servo controller",
+        nullptr,
+        &servo_function,
+        nullptr,
     };
 
-    if (const esp_err_t err{esp_console_cmd_register(&servo_command)}; err != ESP_OK) {
-        fprintf(stderr, "Unable to register servo command in the console");
-    }
-
-    constexpr esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-
-    if (const esp_err_t err{esp_console_new_repl_uart(&hw_config, &repl_config, repl.get())}; err != ESP_OK) {
-        fprintf(stderr, "Unable to establish REPL environment");
-    }
-
-    if (const esp_err_t err{esp_console_start_repl(*repl)}; err != ESP_OK) {
-        fprintf(stderr, "Unable to start REPL environment");
-    }
+    chimera::console::register_command(servo_command);
 }
