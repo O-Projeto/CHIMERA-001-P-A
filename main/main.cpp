a2d2e0c6ebcfc1680cc6e05105501209e95faba4
artofcomputing <pedro.mello@artofcomputing.art>
2024-01-05 20:19:42 -0300

    refactor main to use extracted command

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,8 @@
 //
 // Created by Braum on 23/12/23.
+
 //
+
 extern "C" {
 #include "esp_event.h"
 #include "esp_console.h"
@@ -9,32 +11,19 @@ extern "C" {
 #include <cstdio>
 #include <memory>
 
+#include "command/cxx_command.hpp"
 #include "console/cxx_console.hpp"
 #include "esp_event_cxx.hpp"
 #include "storage/cxx_nvs.hpp"
 #include "exception/cxx_exception.hpp"
 
-/*
- * This is the command entrypoint function, change the code below to make any modifications.
- * NOTE: The console component requires the function signature to be int (int argc, char** argv),
- * without this signature the console will be unable to register the function, and the code won't
- * compile.
- */
-int servo_function(int argc, char** argv) {
-    if (argv[1]) {
-        auto value = std::stoi(argv[1]);
-        value++;
-        fprintf(stdout, "%d\n", value);
-    }
-    else {
-        fprintf(stdout, "missing argument, command won't run\n");
-    }
-    return 0;
-}
-
 // Main task called by FreeRTOS Scheduler after Application Startup
 // https://docs.espressif.com/projects/esp-idf/en/v5.1.2/esp32/api-guides/startup.html
 extern "C" void app_main(void) {
+    /*
+     * TODO: Implement Console Destructor
+    */
+
     // CHIMERA subsystem initialization
     try {
         chimera::nvs subsystem_nvs{};
@@ -47,14 +36,10 @@ extern "C" void app_main(void) {
         abort();
     }
 
-    // Register servo command
-    constexpr esp_console_cmd_t servo_command{
-        "servo",
-        "Run servo controller",
-        nullptr,
-        &servo_function,
-        nullptr,
-    };
-
-    chimera::console::register_command(servo_command);
+    // Register all commands to be used in the console
+    for (const std::array<const esp_console_cmd_t, 1> command_list{
+             chimera::command::servo_command()
+         }; const auto&command: command_list) {
+        chimera::console::register_command(command);
+    }
 }
