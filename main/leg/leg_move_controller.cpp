26667a38a2d29d7663915b87d38241ec7f47bc24
alecremer <ale.cremer.dev@gmail.com>
2024-01-08 21:08:32 -0300

    leg move controller: add move servo, move by ik

--- main/leg/leg_move_controller.cpp
+++ main/leg/leg_move_controller.cpp
@@ -6,11 +6,26 @@
 #include "leg/ik.hpp"
 #include "leg/servo_drive.hpp"
 #include <array>
+#include <functional>
 
 
 void leg_move_controller::move(std::array<float, 3>& position){
 
     const std::array<float, 3> servo_angles = ik_servo.getAngles(position, link_0_length, link_1_length);
-    // drive.Move(servo_angles);
+
+    drive_0.Move(servo_angles.at(0));
+    drive_1.Move(servo_angles.at(1));
+    drive_2.Move(servo_angles.at(2));
+
+}
+
+void leg_move_controller::move_servo_0(const float &angle){ move_servo_x(drive_0, angle);}
+void leg_move_controller::move_servo_1(const float &angle){ move_servo_x(drive_1, angle);}
+void leg_move_controller::move_servo_2(const float &angle){ move_servo_x(drive_2, angle);}
+
+explicit void leg_move_controller::move_servo_x(servo_drive &_drive, const float &_angle){
+
+    int angle_int = (int) _angle; 
+    _drive.Move(angle_int);
 
 }
--- main/leg/leg_move_controller.hpp
+++ main/leg/leg_move_controller.hpp
@@ -18,22 +18,29 @@ class leg_move_controller
 private:
 
     ik ik_servo;
-    servo_drive drive;
+    servo_drive drive_0, drive_1, drive_2;
     float link_0_length, link_1_length;
+    void move_servo_x(servo_drive &_drive, const float &_angle);
 
 
     
 public:
 
-    leg_move_controller(const int32_t gpio_servo, const float &_link_0_length, const float &_link_1_length) 
-    : drive(gpio_servo)
+    leg_move_controller(const int32_t gpio_servo_0, const int32_t gpio_servo_1, const int32_t gpio_servo_2,
+     const float &_link_0_length, const float &_link_1_length) 
+    : drive_0(gpio_servo_0), drive_1(gpio_servo_1), drive_2(gpio_servo_2)
     {
 
         link_0_length = _link_0_length;
         link_1_length = _link_1_length;
+        
     }
     void move(std::array<float, 3>& position);
+    void move_servo_0(const float &angle);
+    void move_servo_1(const float &angle);
+    void move_servo_2(const float &angle);
    
 };
+
 
 #endif
