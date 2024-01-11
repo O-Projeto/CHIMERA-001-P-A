23f6ad20f8834905effe7cd035b03235c1e0eb45
artofcomputing <pedro.mello@artofcomputing.art>
2024-01-11 19:43:49 -0300

    formatting servo_drive interface

--- main/leg/servo_drive.hpp
+++ main/leg/servo_drive.hpp
@@ -6,34 +6,41 @@
 #define SERVO_DRIVER_HPP
 
 extern "C" {
-
-#include "esp_log.h"
 #include "driver/mcpwm_prelude.h"
-
 }
 
-class servo_drive
-{
-private:
-
-    int32_t gpio_servo;
-
+class servo_drive {
 public:
+    explicit servo_drive(int32_t gpio_servo);
 
-    mcpwm_timer_handle_t timer; // The time base of the final PWM signal. It also determines the event timing of other submodules
+    ~servo_drive();
+
+    mcpwm_timer_handle_t timer;
+
+    // The time base of the final PWM signal. It also determines the event timing of other submodules
     mcpwm_timer_config_t timer_config;
-    mcpwm_oper_handle_t oper; // operator: generate PWM waveform
+
+    // operator: generate PWM waveform
+    mcpwm_oper_handle_t oper;
+
     mcpwm_operator_config_t operator_config;
-    mcpwm_cmpr_handle_t comparator; // comparator: compare base time with configured value
+
+    // comparator: compare base time with configured value
+    mcpwm_cmpr_handle_t comparator;
+
     mcpwm_comparator_config_t comparator_config;
-    mcpwm_gen_handle_t generator; // can generate a pair of PWM waves complementary or independently 
+
+    // can generate a pair of PWM waves complementary or independently
+    mcpwm_gen_handle_t generator;
+
     mcpwm_generator_config_t generator_config;
 
-    servo_drive(const int32_t _gpio_servo);
-    ~servo_drive();
     int32_t getGpio();
 
     void Move(int angle);
+
+private:
+    int32_t internal_gpio_servo;
 };
 
 #endif
