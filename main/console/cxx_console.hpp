3351e86777d4b9111b42e005d4ffdbc79647065b
artofcomputing <pedro.mello@artofcomputing.art>
2024-01-11 19:21:49 -0300

    re-organize console data

--- main/console/cxx_console.hpp
+++ main/console/cxx_console.hpp
@@ -27,7 +27,6 @@ namespace chimera {
         static void register_command(const esp_console_cmd_t& command);
 
     private:
-        std::unique_ptr<esp_console_repl_t *> console_ptr;
         const esp_console_repl_config_t console_config{
             32,
             nullptr,
@@ -36,6 +35,7 @@ namespace chimera {
             "CHIMERA-001-P-A >",
             0
         };
+        std::unique_ptr<esp_console_repl_t *> console_ptr;
     };
 }
 
