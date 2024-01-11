c148ade08e19df1c486a911e447478d38b92476b
artofcomputing <pedro.mello@artofcomputing.art>
2024-01-11 19:43:40 -0300

    formatting servo_drive implementation

--- main/leg/servo_drive.cpp
+++ main/leg/servo_drive.cpp
@@ -12,18 +12,16 @@
  */
 
 extern "C" {
-
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/mcpwm_prelude.h"
-
 }
 
 #include <iostream>
-#include "leg/servo_drive.hpp"
-using namespace std;
 
+#include "leg/servo_drive.hpp"
+
+using namespace std;
 
 
 // Please consult the datasheet of your servo before changing the following parameters
@@ -36,27 +34,26 @@ using namespace std;
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
-static const char *TAG = "Debug";
+static const char* TAG = "Debug";
 bool debug = true;
 
-static inline uint32_t example_angle_to_compare_2(int angle)
-{
-    return (angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (SERVO_MAX_DEGREE - SERVO_MIN_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
+static uint32_t example_angle_to_compare_2(int angle) {
+    return (angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (
+               SERVO_MAX_DEGREE - SERVO_MIN_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
 }
 
-servo_drive::servo_drive(const int32_t _gpio_servo)
-{
+servo_drive::servo_drive(const int32_t gpio_servo) {
+    internal_gpio_servo = gpio_servo;
 
-    gpio_servo = gpio_servo;
-    
-    if(debug) ESP_LOGI(TAG, "Create timer and operator");
+    if (debug)
+        ESP_LOGI(TAG, "Create timer and operator");
     timer_config.group_id = 0;
     timer_config.clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT;
     timer_config.resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ;
     timer_config.period_ticks = SERVO_TIMEBASE_PERIOD;
     timer_config.count_mode = MCPWM_TIMER_COUNT_MODE_UP;
     ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
-    
+
 
     mcpwm_oper_handle_t _oper = NULL;
     mcpwm_operator_config_t _operator_config = mcpwm_operator_config_t();
@@ -69,70 +66,47 @@ servo_drive::servo_drive(const int32_t _gpio_servo)
     ESP_ERROR_CHECK(mcpwm_new_operator(&(operator_config), &(oper)));
 
 
-    if(debug) ESP_LOGI(TAG, "Connect timer and operator");
+    if (debug)
+        ESP_LOGI(TAG, "Connect timer and operator");
     ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
 
-    if(debug) ESP_LOGI(TAG, "Create comparator and generator from the operator");
+    if (debug)
+        ESP_LOGI(TAG, "Create comparator and generator from the operator");
     comparator = NULL;
     comparator_config = mcpwm_comparator_config_t();
     // operator_config = operator_config;
     comparator_config.flags.update_cmp_on_tez = true;
-    
+
     ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &(comparator_config), &(comparator)));
 
     generator = NULL;
     generator_config = mcpwm_generator_config_t();
-    generator_config.gen_gpio_num = gpio_servo;
+    generator_config.gen_gpio_num = internal_gpio_servo;
 
     ESP_ERROR_CHECK(mcpwm_new_generator(oper, &(generator_config), &(generator)));
 
     // set the initial compare value, so that the servo will spin to the center position
     ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare_2(0)));
 
-    if(debug) ESP_LOGI(TAG, "Set generator action on timer and compare event");
+    if (debug)
+        ESP_LOGI(TAG, "Set generator action on timer and compare event");
     // go high on counter empty
     ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generator,
-                                                              MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
     // go low on compare threshold
     ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
-                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
 
-    if(debug) ESP_LOGI(TAG, "Enable and start timer");
+    if (debug)
+        ESP_LOGI(TAG, "Enable and start timer");
     ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
     ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
-    
 }
 
-servo_drive::~servo_drive()
-{
-}
-
-
-
-void servo_drive::Move(int angle){
-
-    if(debug) ESP_LOGI(TAG, "Moving... %i", angle);
+void servo_drive::Move(int angle) {
+    if (debug)
+        ESP_LOGI(TAG, "Moving... %i", angle);
     ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare_2(angle)));
-    
 }
 
-int32_t servo_drive::getGpio(){ return gpio_servo; }
-
-
-
-// extern "C" void app_main(void)
-// {
-    
-//     servo_drive servo = servo_drive(32);
-//     while(1) {
-
-// 		int angle_1 = 0;
-// 		int angle_2 = 60;
-//         int time_sleep = 500;
-        
-//         servo.Move(angle_1);
-//         vTaskDelay(time_sleep);
-//         servo.Move(angle_2);
-//         vTaskDelay(time_sleep);
-//     }
-// }
+int32_t servo_drive::getGpio() { return internal_gpio_servo; }
